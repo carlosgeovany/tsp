5f75aeebb11f5d3da395b06f2d007ae9862b81e6
carlosgeovany <ccggppvv@hotmail.com>
2020-11-08 14:46:14 -0600

    create empty file for test utils.py related to issue #5

---
+++ tests/test_utils.py
@@ -0,0 +1,18 @@
+import pytest
+
+from tsp.utils import process_grid, load_algorithm, flatten_algorithm_grid
+from config import settings
+
+
+grids = settings.algorithms
+
+def test_process_grid(grid):
+    pass
+
+
+def test_load_algorithm():
+    pass
+
+
+def test_flatten_algorithm_grid():
+    pass
